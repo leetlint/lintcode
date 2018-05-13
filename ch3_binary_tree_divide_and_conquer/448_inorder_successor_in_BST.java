3c00e5322287875c3a5a198b1a64c68c825147d3
z76316 <z7631614@gmail.com>
2018-05-14 00:51:19 +0800

    don't be lazy

---
+++ ch3_binary_tree_divide_and_conquer/448_inorder_successor_in_BST.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: The root of the BST.
+     * @param p: You need find the successor node of p.
+     * @return: Successor of p.
+     */
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        // write your code here
+        TreeNode successor = null;
+        while (root != null && root.val != p.val) {
+            if (root.val > p.val) {
+                successor = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        
+        if (root == null) return null;
+        
+        if (root.right == null) return successor;
+        
+        root = root.right;
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+}
